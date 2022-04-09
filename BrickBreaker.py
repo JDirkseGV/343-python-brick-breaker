98b6157db2cf7ce160fd016a3da97e0fa3cc114a
John <dirksej@mail.gvsu.edu>
2022-04-09 12:03:06 -0400

    marked private instances as private with __

--- BrickBreaker.py
+++ BrickBreaker.py
@@ -10,18 +10,18 @@ class Overlay(pg.sprite.Sprite):
         super().__init__()
         self.font = pg.font.SysFont(font, size)
         self.color = color
-        self.text = text
+        self.__text = text
         self.screen = screen
         self.antialias = antialias
         self.pos = pos
-        self.rerender()
+        self.__rerender()
         
     def setText(self, text): #sets self.text
-        self.text = text
-        self.rerender()
+        self.__text = text
+        self.__rerender()
         
-    def rerender(self): #renders self.text at specified position
-        self.image = self.font.render(self.text, self.antialias, self.color)
+    def __rerender(self): #renders self.text at specified position
+        self.image = self.font.render(self.__text, self.antialias, self.color)
         self.rect = self.image.get_rect()
         self.rect.x = self.pos[0]
         self.rect.y = self.pos[1]
@@ -36,14 +36,14 @@ class Paddle(pg.sprite.Sprite):
         self.rect.x = 400
         self.rect.y = 550
         self.image.fill((0,0,0))
-        self.speed = 15
+        self.__speed = 15
         
     def update(self): #Move paddle in a linear motion along the bottom of the screen with arrow keys
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= self.speed
+            self.rect.x -= self.__speed
         if keys[pg.K_RIGHT] and self.rect.right < 800:
-            self.rect.x += self.speed
+            self.rect.x += self.__speed
 
 #Block object is a stationary collidable sprite that is destroyable in game.
 #It regulates its death condition and deletes itself when out of hp.
@@ -59,14 +59,14 @@ class Block(pg.sprite.Sprite):
         self.g = r(0, 255)
         self.b = r(0, 255)
         self.image.fill((self.r,self.g,self.b))
-        self.hp = self.life()
+        self.__hp = self.__life()
     
-    def life(self): #determines how much hp the block should have based on its color
+    def __life(self): #determines how much hp the block should have based on its color
         return 766 - self.r - self.g - self.b
         
     def hit(self): #Called when a ball collides with block, handles hp and color change based on losing hp
-        self.hp -= 24 #24 per hit instead of 25 as per directions because 24/3 = 8
-        if self.hp <= 0:
+        self.__hp -= 24 #24 per hit instead of 25 as per directions because 24/3 = 8
+        if self.__hp <= 0:
             self.kill() #Also deletes itself when no more hp
 
         if self.r <= 246:
@@ -84,14 +84,14 @@ class Ball(pg.sprite.Sprite):
     blocks = None
     def __init__(self):
         super().__init__()
-        self.SIZE = 23
-        self.image = pg.Surface((self.SIZE,self.SIZE))
+        self.__SIZE = 23
+        self.image = pg.Surface((self.__SIZE,self.__SIZE))
         self.image.fill((255, 255, 255))
-        pg.draw.circle(self.image, (0, 101, 164), (self.SIZE/2, self.SIZE/2), self.SIZE/2)
+        pg.draw.circle(self.image, (0, 101, 164), (self.__SIZE/2, self.__SIZE/2), self.__SIZE/2)
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 400
-        self.velocity = [3,-3]
+        self.__velocity = [3,-3]
         self.blocks = None
         self.paddle = None
         self.boom = pg.mixer.Sound(file = "Boom.wav")
@@ -99,55 +99,54 @@ class Ball(pg.sprite.Sprite):
     #update ball each frame, fed game as a parameter so that it can call functions to change game variables
     def update(self, game):
         #Move ball this frame by velocity
-        self.rect.x += self.velocity[0]
-        self.rect.y += self.velocity[1]
+        self.rect.x += self.__velocity[0]
+        self.rect.y += self.__velocity[1]
         #Handle bouncing off walls
         if self.rect.x < 0:
-            self.rect.x -= 1*self.velocity[0]-1
-            self.rect.y -= 1*self.velocity[1]-1
-            self.velocity[0] = -self.velocity[0]
-        if self.rect.x > 800 - self.SIZE:
-            self.rect.x -= 1*self.velocity[0]-1
-            self.rect.y -= 1*self.velocity[1]-1
-            self.velocity[0] = -self.velocity[0]
+            self.rect.x -= 1*self.__velocity[0]-1
+            self.rect.y -= 1*self.__velocity[1]-1
+            self.__velocity[0] = -self.__velocity[0]
+        if self.rect.x > 800 - self.__SIZE:
+            self.rect.x -= 1*self.__velocity[0]-1
+            self.rect.y -= 1*self.__velocity[1]-1
+            self.__velocity[0] = -self.__velocity[0]
         if self.rect.y < 0:
-            self.rect.x -= 1*self.velocity[0]-1
-            self.rect.y -= 1*self.velocity[1]-1
-            self.velocity[1] = -self.velocity[1]
+            self.rect.x -= 1*self.__velocity[0]-1
+            self.rect.y -= 1*self.__velocity[1]-1
+            self.__velocity[1] = -self.__velocity[1]
 
         #If ball hits bottom of screen, take a life from game and remove ball
-        if self.rect.y > 600 - self.SIZE:
+        if self.rect.y > 600 - self.__SIZE:
             self.kill()
             game.takeLife()
         
         for paddle in Ball.paddle: #Handle paddle collison
             if pg.Rect.colliderect(self.rect, paddle): #using built in collision detection
-                self.rect.x -= (1*self.velocity[0]+1)
-                self.rect.y -= (1*self.velocity[1]+1)
-                self.velocity[1] *= -1.03
-                self.velocity[0] *= 1.03
+                self.rect.x -= (1*self.__velocity[1]+1)
+                self.__velocity[1] *= -1.03
+                self.__velocity[0] *= 1.03
 
         for block in Ball.blocks: #Handle block collision
             #if ball collides with block
             if pg.Rect.colliderect(self.rect, block): #using built in collision detection
                 #handle colliding with top/bottom or a side
                 if self.rect.right > block.rect.x  and self.rect.left < block.rect.x + 100:
-                    self.rect.x -= (1*self.velocity[0]-1)
-                    self.rect.y -= (1*self.velocity[1]-1)
-                    self.velocity[1] *= -1.03
-                    self.velocity[0] *= 1.03  
-                elif self.rect.centery > ((block.rect.top - self.SIZE/2)+1) and self.rect.centery < ((block.rect.bottom + self.SIZE/2)-1):
-                    self.rect.x -= (1*self.velocity[0]-1)
-                    self.rect.y -= (1*self.velocity[1]-1)
-                    self.velocity[0] *= -1.03
-                    self.velocity[1] *= 1.03
+                    self.rect.x -= (1*self.__velocity[0]-1)
+                    self.rect.y -= (1*self.__velocity[1]-1)
+                    self.__velocity[1] *= -1.03
+                    self.__velocity[0] *= 1.03  
+                elif self.rect.centery > ((block.rect.top - self.__SIZE/2)+1) and self.rect.centery < ((block.rect.bottom + self.__SIZE/2)-1):
+                    self.rect.x -= (1*self.__velocity[0]-1)
+                    self.rect.y -= (1*self.__velocity[1]-1)
+                    self.__velocity[0] *= -1.03
+                    self.__velocity[1] *= 1.03
                 else: #handles edge cases by at least doing something to keep ball in bounds if collision code fails
-                    self.rect.x -= (1*self.velocity[0]-1)
-                    self.rect.y -= (1*self.velocity[1]-1)
-                    self.velocity[1] *= -1.03
-                    self.velocity[0] *= -1.03
-                self.velocity[0] = min(12, self.velocity[0]) #cap velocity at 12
-                self.velocity[1] = min(12, self.velocity[1])
+                    self.rect.x -= (1*self.__velocity[0]-1)
+                    self.rect.y -= (1*self.__velocity[1]-1)
+                    self.__velocity[1] *= -1.03
+                    self.__velocity[0] *= -1.03
+                self.__velocity[0] = min(12, self.__velocity[0]) #cap velocity at 12
+                self.__velocity[1] = min(12, self.__velocity[1])
                 self.boom.play()
                 block.hit()
                 game.addScore()
@@ -163,15 +162,15 @@ class Game:
         pg.mixer.music.load("background.wav")
         pg.mixer.music.play(-1, 0.0)
         self.__running = False
-        self.score = 0
-        self.lives = 10
+        self.__score = 0
+        self.__lives = 10
         self.screen = pg.display.set_mode( (800, 600) )
         self.clock = pg.time.Clock()
         self.balls = pg.sprite.Group() #__ to mark private instance variables!
         self.blocks = pg.sprite.Group()
         self.paddle = pg.sprite.Group()
         self.sprites = pg.sprite.RenderUpdates()
-        self.infoText = Overlay(self.screen, 'Score: '+ str(self.score) + '   Lives: ' + str(self.lives),
+        self.infoText = Overlay(self.screen, 'Score: '+ str(self.__score) + '   Lives: ' + str(self.__lives),
             (0,570), "Arial", 24, (0,0,0), True)
         self.sprites.add(self.infoText)
 
@@ -200,14 +199,14 @@ class Game:
             if blocks <= 0: #Win/lose conditions else keep displaying lives and score
                 for ball in self.balls:
                     ball.kill()
-                self.infoText.setText("You Win! Score: " + str(self.score))
-            elif self.lives <= 0:
+                self.infoText.setText("You Win! Score: " + str(self.__score))
+            elif self.__lives <= 0:
                 for ball in self.balls:
                     ball.kill()
                 self.infoText.setText('GAME OVER, EXIT AND RELAUNCH TO RESTART')
             else:
                 self.infoText.setText(
-                    'Score: ' + str(self.score) + '   Lives: ' + str(self.lives))
+                    'Score: ' + str(self.__score) + '   Lives: ' + str(self.__lives))
                 
             # Redraw
             self.screen.fill( (255, 255, 255) )
@@ -219,10 +218,10 @@ class Game:
             self.clock.tick(60)
         
     def addScore(self):
-        self.score += 25
+        self.__score += 25
     
     def takeLife(self):
-        self.lives -= 1
+        self.__lives -= 1
 
     def setRunning(self, running):
         self.__running = running
